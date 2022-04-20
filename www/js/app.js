05d2764ea32caf2ebe16c809c574ca50c41d4cc0
Laydemir <voldvakoff@gmail.com>
2022-04-20 10:12:55 +0300

    заливка на GitHub

---
+++ www/js/app.js
@@ -0,0 +1,67 @@
+$(function() {
+/* Fixed header*/
+	let header = $("#header");
+	let intro = $("#intro");
+	let introH = intro.innerHeight();
+	let scrollPos = $(window).scrollTop();
+	let nav = $("#nav");
+	let navToggle = $("#navToggle");
+
+	checkScroll(scrollPos, introH);
+
+	$(window).on("scroll resize", function(){
+		introH = intro.innerHeight ();
+		scrollPos = $(this).scrollTop();
+
+		checkScroll(scrollPos, introH);
+	});
+	function checkScroll(scrollPos, introH) {
+		if(scrollPos > introH ) {
+			header.addClass("fixed");
+		} else {
+			header.removeClass("fixed");
+		}
+	}
+
+
+	/* smooth scroll*/
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
+		let elementID = $(this).data('scroll');
+		let elementOffset = $(elementID).offset().top;
+
+		nav.removeClass("show");
+
+		$("html, body").animate({
+			scrollTop: elementOffset - 70
+		}, 1000 /*время длительности скрола в милисекундах*/);
+
+	});
+
+	/* Nav Toggle*/
+	$("#navToggle").on("click", function(event) {
+		event.preventDefault();
+
+		nav.toggleClass("show");
+
+	});
+
+/* Coment: https://kenwheeler.github.io/slick/*/
+	let slider = $("#comentSlider");
+
+	slider.slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		arrows: false,
+		dots: true
+	});
+
+
+	});
+
+	
+
+	
+	
